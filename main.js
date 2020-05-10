b40cbbc11024869d81f470130a3f7aaf2dfb474f
Adarsh Kumar <ideal.ak1308@gmail.com>
2020-05-10 18:56:29 +0530

    Reimplement: Check Available Video Devices

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
-navigator.mediaDevices.getUserMedia({video:true}).then(function(stream){
+'use strict';
 
-    // vid.onloadedmetadata = function(){
-    //   this.width = overlay.width = this.videoWidth;
-    //   this.height = overlay.height = this.videoHeight;
-    //   }
-    vid.srcObject = stream;
-    vid.play();
-    overlay.onclick = function(){
-      var c = document.createElement('canvas');
-      c.width = vid.videoWidth;
-      c.height = vid.videoHeight;
-      c.getContext('2d').drawImage(vid, 0,0);
-      c.toBlob(doWhatYouWantWithTheCapturedImage);
-      };
-    });
-  
-  function doWhatYouWantWithTheCapturedImage(blob){
-    var url = URL.createObjectURL(blob);
-    var img = new Image();
-    img.onload = function(){URL.revokeObjectURL(url);};
-    img.src = url;
-    URL.revokeObjectURL(vid.src);
-    overlay.parentNode.appendChild(img);
-    vid.parentNode.removeChild(vid);
-    overlay.parentNode.removeChild(overlay);
+var videoElement = document.querySelector('video');
+var videoSelect = document.querySelector('select#videoSource');
+
+videoSelect.onchange = getStream;
+
+getStream().then(getDevices).then(gotDevices);
+
+function getDevices() {
+  // AFAICT in Safari this only gets default devices until gUM is called :/
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function gotDevices(deviceInfos) {
+  window.deviceInfos = deviceInfos; // make available to console
+  console.log('Available input and output devices:', deviceInfos);
+  for (const deviceInfo of deviceInfos) {
+    const option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
+      videoSelect.appendChild(option);
     }
+  }
+}
+
+function getStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+  const videoSource = videoSelect.value;
+  const constraints = {
+    video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+  };
+  return navigator.mediaDevices.getUserMedia(constraints).
+    then(gotStream).catch(handleError);
+}
+
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  videoSelect.selectedIndex = [...videoSelect.options].
+    findIndex(option => option.text === stream.getVideoTracks()[0].label);
+  videoElement.srcObject = stream;
+}
+
+function handleError(error) {
+  console.error('Error: ', error);
+}
+
